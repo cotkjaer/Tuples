4bbbf270fe3b9d992fb2856e008d11ee32e2ca39
christian otkjær <christian.otkjaer@gmail.com>
2016-03-04 10:08:30 +0100

    Initial Commit

---
+++ Tuples/Tuples.h
@@ -0,0 +1,19 @@
+//
+//  Tuples.h
+//  Tuples
+//
+//  Created by Christian Otkjær on 04/03/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tuples.
+FOUNDATION_EXPORT double TuplesVersionNumber;
+
+//! Project version string for Tuples.
+FOUNDATION_EXPORT const unsigned char TuplesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tuples/PublicHeader.h>
+
+
